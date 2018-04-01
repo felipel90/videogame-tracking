d0227f7b5d13f684226418da2b770a2945cea1ef
Felipe Lopes <felipe.webdev@gmail.com>
2018-04-01 18:09:26 -0300

    Remove some services

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase/app';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { MatSnackBar } from '@angular/material';
+import { ConsolesService } from '../../services/consoles/consoles.service';
 
 @Component({
   selector: 'app-home',
@@ -11,25 +12,24 @@ import { MatSnackBar } from '@angular/material';
 
 export class HomePage implements OnInit {
   gameData = {};
+  consoles: any[];
   todoCollectionRef: AngularFirestoreCollection<any>;
   activeProgressBar = false;
 
   constructor(
-    private afs: AngularFirestore,
-    public snackBar: MatSnackBar) { }
+    private _afs: AngularFirestore,
+    public snackBar: MatSnackBar,
+    private _consolesService: ConsolesService) { 
+      this._consolesService.getConsoleList().subscribe((res) => {
+        this.consoles = res;        
+      })
+    }
 
-  ngOnInit() { }
-
-  consoles = [
-    { value: 'Ps1' },
-    { value: 'Ps2' },
-    { value: 'Ps3' },
-    { value: 'Nitendo' }
-  ];
+  ngOnInit() { }  
 
   public createNewGame() {
     this.activeProgressBar = true;
-    return this.afs.collection('games').add(this.gameData)
+    return this._afs.collection('games').add(this.gameData)
       .then((res) => {
         this.gameData = {};
         this.activeProgressBar = false;
